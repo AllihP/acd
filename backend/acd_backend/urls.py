16bdc6bee9af9cc8e5108f2a534ad273c09a3f95
AllihP <hillaprincebambe@gmail.com>
2026-04-23 09:47:11 +0100

    FIN6

--- backend/acd_backend/urls.py
+++ backend/acd_backend/urls.py
@@ -1,11 +1,21 @@
-from django.views.generic import TemplateView
+from django.contrib import admin  # <-- CET IMPORT MANQUAIT
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('doulgue/', admin.site.urls), # Votre admin secret
+    # Votre accès secret pour l'admin
+    path('doulgue/', admin.site.urls), 
+    
+    # Points d'accès de l'API
     path('api/core/', include('apps.core.urls')),
     path('api/contact/', include('apps.contact.urls')),
     
-    # On sert l'index.html qui se trouve dans STATIC_ROOT (celui collecté)
+    # Sert l'index de React pour toutes les autres routes (Frontend)
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Service des médias uniquement en mode debug (local)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
