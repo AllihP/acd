9fe5d8caf5ff93e3d175a551036147e964a0889c
AllihP <hillaprincebambe@gmail.com>
2026-04-23 09:44:06 +0100

    FIN5

--- backend/build.sh
+++ backend/build.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Build du Frontend
-cd frontend
-npm install
-npm run build # Ici Vite crée le dossier 'dist'
-cd ..
+# Définition de la racine
+ROOT_DIR=$(dirname "$(cd "$(dirname "$0")" && pwd)")
 
-# 2. Préparation du Backend
-cd backend
+echo "🚀 [1/3] Build du Frontend..."
+cd "$ROOT_DIR/frontend"
+npm install
+# FORCER LA PERMISSION ICI
+chmod +x node_modules/.bin/vite
+npm run build 
+
+echo "🐍 [2/3] Installation Django..."
+cd "$ROOT_DIR/backend"
 pip install -r requirements.txt
 
-# 3. La fusion (Collectstatic)
-# Django va prendre le 'index.html' de React et le mettre dans 'staticfiles'
+echo "📂 [3/3] Collecte et Nettoyage des Statiques..."
+# Le flag --clear est vital pour supprimer les vieux fichiers index.html erronés
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
+
+echo "✅ Build ACD réussi !"
