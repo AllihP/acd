eaa4b741ff38bd0af465495a332934af3ea6f39f
AllihP <hillaprincebambe@gmail.com>
2026-04-23 08:35:38 +0100

    fix requirements et render16

--- backend/build.sh
+++ backend/build.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env bash
-# Arrêter le script en cas d'erreur
 set -o errexit
 
-# --- 1. BUILD FRONTEND ---
 echo "Building Frontend..."
 cd frontend
 npm install
+
+# --- AJOUTER CETTE LIGNE POUR FIXER LES PERMISSIONS ---
+chmod +x node_modules/.bin/vite
+
 npm run build
 cd ..
 
-# --- 2. PREPARATION BACKEND ---
 echo "Building Backend..."
 cd backend
 pip install -r requirements.txt
 
-# On déplace les fichiers compilés de React vers un dossier que Django peut voir
-mkdir -p staticfiles
-cp -r ../frontend/dist/* ./staticfiles/
-
-# Migrations et Statiques
-python manage.py migrate
+# Collecte des fichiers de React (dist) vers Django
 python manage.py collectstatic --no-input
+python manage.py migrate
 
-# Création de l'admin (URL secrète déjà configurée dans vos logs)
+# Création de l'admin secret (doulgue)
 if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser \
     --no-input \
